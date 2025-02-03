10d0de4cfa8bc0cf046932f6ca0c87d4712427c1
kpetrovi <kpetrovi@emich.edu>
2025-02-03 12:21:16 -0500

    programmer 2

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -3,5 +3,6 @@ package eclipseSoloProjA;
 public class HelloWorld {
 	public static void main (String [] args) {
 		System.out.println("Hello");
+		System.out.println("'Programmer 2 did this");
 	}
 }
